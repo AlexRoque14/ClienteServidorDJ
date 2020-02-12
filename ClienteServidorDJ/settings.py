3b8514fd8ca32dfc94f593621599bb44f9bfb57f
AlexRoque14 <171048@ids.upchiapas.edu.mx>
2020-02-11 21:01:02 -0600

    Metodo post

--- ClienteServidorDJ/settings.py
+++ ClienteServidorDJ/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from decouple import config, Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')sp0nsig)!h8q**49*cwn=nabktb9rvk@t5-hgda4ylzo7%)qg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost','3.86.115.93']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast= Csv())
 
 
 # Application definition
@@ -95,10 +95,10 @@ WSGI_APPLICATION = 'ClienteServidorDJ.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbalexis',
-        'USER': 'postgres',
-        'PASSWORD': 'master1234',
-        'HOST': 'database-1.ckljaavzngqk.us-east-1.rds.amazonaws.com',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '5432',
     }
 }
